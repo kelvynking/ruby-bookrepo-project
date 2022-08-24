0e9f8fd13108f821e04dd76c992db07f9f9718e5
Kelvyn King <kelvyn.m.king@gmail.com>
2022-08-24 14:29:21 +1000

    Establish connection to database.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
 #import items in 'Gemfile'
 require 'bundle/setup'
 Bundler.require
+
+#create connection to database
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/development.sqlite3'
+)
