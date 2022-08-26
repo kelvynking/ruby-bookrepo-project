0467253072a028d3546e2fb30f9eb8ea5f99bde9
Kelvyn King <kelvyn.m.king@gmail.com>
2022-08-26 12:13:35 +1000

    Added Genre class and migration CreateGenres class.

--- app/scraper.rb
+++ app/scraper.rb
@@ -8,5 +8,15 @@ module Scraper
         def find_genres
             load_homepage.css('#browseBox .gr-hyperlink')
         end
+
+        def add_genres_to_database
+             puts 'Loading genres into the database. Please wait ...'
+            find_genres.each do |g|
+                new_genre = g.text.strip
+                Genre.find_or_create_by(name: new_genre) do |genre|
+                    genre.name = new_genre
+                end
+            end
+        end
     end
 end
